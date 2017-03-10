dabd48a80606f663828290f5f1725b16d57f944b
Ernane Sena <ernane.sena@gmail.com>
2017-03-09 23:42:06 -0300

    Mapeamento de configuração da entidade Book já salvando no banco de dados

---
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
@@ -0,0 +1,25 @@
+package br.com.casadocodigo.loja.managedbeans;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import br.com.casadocodigo.loja.daos.BookDAO;
+import br.com.casadocodigo.loja.models.Book;
+
+@Model
+public class AdminBooksBean {
+
+	private Book product = new Book();
+	@Inject
+	private BookDAO bookDAO;
+
+	@Transactional
+	public void save() {
+		bookDAO.save(product);
+	}
+
+	public Book getProduct() {
+		return product;
+	}
+}
